cfaead30d45f74602f38410371bca247ae284c7a
Breno Batista Campos <brenobatistacampos@gmail.com>
2023-12-20 15:47:43 +0000

    atualizando projeto para ser baseado em json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,25 @@
-const acordeonTriggers = document.querySelectorAll(".acordeon .trigger");
+function updateProfileData(profileData) {
+    const fotoDePerfil = document.querySelector('.app-header-photo');
+    const titulo = document.querySelector('.app-header-title');
+    const job = document.querySelector('.app-header-details .job');
+    const profileLocation = document.querySelector('.app-header-details .loc');
+    const phoneNumber = document.querySelector('.app-header-details .number');
+    const email = document.querySelector('.app-header-details .email');
+    fotoDePerfil.setAttribute("src", profileData.photo)
+    fotoDePerfil.setAttribute("alt", `Foto de ${profileData.name}`)
+    titulo.innerHTML += `${profileData.name}`
+    job.textContent = profileData.job
+    profileLocation.innerHTML = profileData.location
+    phoneNumber.innerHTML = `<a href="tel:${profileData.phone}">${profileData.phone}</a>`
+    email.innerHTML = `<a href="mailto:${profileData.email}">${profileData.email}</a>`
+}
 
-acordeonTriggers.forEach((trigger) => {
-    trigger.addEventListener('click', (e) => {
-        const acordeon = trigger.parentElement
-        const isOpen = acordeon.classList.contains('open')
-        
-        if (isOpen) {
-            acordeon.classList.remove('open')
-        } else{
-        acordeon.classList.add('open')
-        }
-    })
-})
+function updateSkills (profileData) {
+}
+
+(async () =>{
+    const profileData = await fetchProfileData()
+    updateProfileData(profileData)
+    updateSkills
+
+})()
