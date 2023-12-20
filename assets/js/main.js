4fa43e789dfd8c6b4a18265086f1f75e66fbb8ee
Breno Batista Campos <brenobatistacampos@gmail.com>
2023-12-20 18:03:44 +0000

    projeto atualizado para estilo api

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,11 +15,70 @@ function updateProfileData(profileData) {
 }
 
 function updateSkills (profileData) {
+    const listaHardSkills = profileData.skills.hardSkills
+    const ulFerramentas = document.querySelector('#ul-tools')
+    
+    const listaSoftSkills = profileData.skills.softSkills
+    const ulSkills = document.querySelector('#personal-skills')
+
+    listaHardSkills.forEach(element => {
+        const newTool = `<li><img src="${element.logo}" alt="${element.name}" title="${element.name}"></li>`
+        ulFerramentas.innerHTML += newTool
+    });
+
+    listaSoftSkills.forEach(element => {
+        const newSkill = `<li>${element}</li>`
+        ulSkills.innerHTML += newSkill
+    });
+
+}
+
+function updateLanguages(profileData) {
+    const languages = document.querySelector('.languages');
+
+    languages.innerHTML = profileData.languages.map(element => {
+        return `<li>${element}</li>`
+    }).join('')
+}
+
+function updatePortfolio (profileData) {
+    const ulPortfolio = document.querySelector('.portfolio')
+    console.log(ulPortfolio)
+    profileData.portfolio.forEach(element => {
+        if (element.github){
+            const newItemPortfolio = `<li>
+        <h3 class="title github">${element.name}</h3>
+        <a href="${element.url}" target="_blank">www.google.com</a>
+        </li>`
+        ulPortfolio.innerHTML += newItemPortfolio
+    } else {
+        const newItemPortfolio = `<li>
+        <h3 class="title">${element.name}</h3>
+        <a href="${element.url}" target="_blank">${element.url}</a>
+        </li>`
+        ulPortfolio.innerHTML += newItemPortfolio
+    }
+    })
+}
+
+function updateExperiencia (profileData) {
+    const ulExperiencia = document.querySelector(".experience");
+    profileData.professionalExperience.forEach(element => {
+        const newExperiencia = document.createElement("li")
+        newExperiencia.innerHTML = `<h3>${element.name}</h3>
+        <span class="period">${element.period}</span>
+        <p>${element.description}</p>`
+        ulExperiencia.appendChild(newExperiencia)
+
+    })
+
 }
 
 (async () =>{
     const profileData = await fetchProfileData()
     updateProfileData(profileData)
-    updateSkills
-
+    updateSkills(profileData)
+    updateLanguages(profileData)
+    updatePortfolio(profileData)
+    updateExperiencia(profileData)
 })()
